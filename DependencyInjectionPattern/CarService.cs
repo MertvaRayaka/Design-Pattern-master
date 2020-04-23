b909de3aa058d57ee301189670c9bef496f40b01
NEIL.ZHU <631203498@qq.com>
2020-04-23 20:23:30 +0800

    移除IOCPattern 增加DIPattern

---
+++ DependencyInjectionPattern/CarService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniDIDemo
+{
+    public class CarService : ICar
+    {
+        public void ShowName(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ DependencyInjectionPattern/UsersService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MiniDIDemo
+{
+    public class UsersService : IUser
+    {
+        public ICar _carService;
+
+        public string Name { get ; set ; }
+
+        public UsersService(ICar carInterface)
+        {
+            _carService = carInterface;
+        }
+
+
+        public void MyName()
+        {
+            Console.WriteLine("我的名字是："+ Name);
+        }
+
+        public void MyCarName(string str)
+        {
+            Console.Write("我的汽车是：");
+            _carService.ShowName(str);
+        }
+
+    }
+}
